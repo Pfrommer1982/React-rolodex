db6f07f6e70a8ddd9c710cf12a7a2c87a55c2b80
Christoph Pfrommer <pfrommer1982@gmail.com>
2022-06-10 10:09:45 +0200

    Made website responsive

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
+
 class App extends Component {
   constructor() {
     super();
@@ -43,7 +44,7 @@ class App extends Component {
         <h1 className= "app-title">Monsters Rolodex</h1>
         <SearchBox
           onChangeHandler={onSearchChange}
-          placeholder={"search"}
+          placeholder={"search for monster"}
           className="monsters-search-box"
         />
         <CardList monsters={filteredMonsters} />
